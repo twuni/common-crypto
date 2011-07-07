761ca836096b9b348627ef6bf23b6e0df373761e
Leo Accend <github.aeonic@spamgourmet.com>
2011-07-06 18:15:02 -0700

    Removing unused StreamProcessor.

--- src/main/java/org/twuni/common/crypto/StreamProcessor.java
+++
@@ -1,8 +0,0 @@
-package org.twuni.common.crypto;
-
-
-public interface StreamProcessor<From,To> {
-	
-	public byte [] process( Transformer<From,To> transformer, byte [] buffer, int offset, int length );
-
-}
