d3984beccc28004c2d4c21f5dab78a0a340a5da6
Leo Accend <github.aeonic@spamgourmet.com>
2011-07-19 12:07:04 -0700

    Moving RSA cryptography to common-crypto-rsa. Adding a message constructor to InputLengthException.

--- src/main/java/org/twuni/common/crypto/InputLengthException.java
+++ src/main/java/org/twuni/common/crypto/InputLengthException.java
@@ -2,4 +2,11 @@ package org.twuni.common.crypto;
 
 public class InputLengthException extends RuntimeException {
 
+	public InputLengthException() {
+	}
+
+	public InputLengthException( String message ) {
+		super( message );
+	}
+
 }
