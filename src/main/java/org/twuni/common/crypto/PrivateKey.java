1f01369a504f29541d558f2af0bed3297eb79eac
Leo Accend <github.aeonic@spamgourmet.com>
2011-08-10 13:40:14 -0700

    Adding interfaces to distinguish between cryptographic public keys and private keys.

---
+++ src/main/java/org/twuni/common/crypto/PrivateKey.java
@@ -0,0 +1,7 @@
+package org.twuni.common.crypto;
+
+import java.math.BigInteger;
+
+public interface PrivateKey extends Transformer<BigInteger, BigInteger> {
+
+}
---
+++ src/main/java/org/twuni/common/crypto/PublicKey.java
@@ -0,0 +1,7 @@
+package org.twuni.common.crypto;
+
+import java.math.BigInteger;
+
+public interface PublicKey extends Transformer<BigInteger, BigInteger> {
+
+}
